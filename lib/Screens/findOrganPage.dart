474dcb6db9b8bfdfd7c2f65f66e468103299f2f5
Chathura Jayakody <chathurajy96@gmail.com>
2025-01-20 01:42:03 +0530

    added Find Organ Page

---
+++ lib/Screens/findOrganPage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Findorganpage extends StatefulWidget {
+
+  const Findorganpage({super.key});
+
+  @override
+  State<Findorganpage> createState() => _FindorganpageState();
+}
+
+class _FindorganpageState extends State<Findorganpage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text("chathura"),
+    );
+  }
+}
